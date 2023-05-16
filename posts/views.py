099c991eff0c4007234267c75ce280f88578e04a
Your Name <you@example.com>
2023-05-16 01:59:57 -0700

    GenericApiView

---
+++ posts/views.py
@@ -0,0 +1,39 @@
+
+from rest_framework import generics
+from rest_framework import mixins
+
+from .models import Post
+from .serializers import PostSerializer
+
+
+
+
+class PostListView(mixins.ListModelMixin,
+                   mixins.CreateModelMixin,
+                    generics.GenericAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def get(self, request,  *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+class PostDetailView(
+                     mixins.RetrieveModelMixin,
+                     mixins.UpdateModelMixin,
+                     mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def get(self, request,  *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
